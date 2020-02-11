078ad0a5522042bce8cad127e5fae439550ae7ea
Donovan <donovan.faglain@ynov.com>
2020-02-12 00:03:41 +0100

    ajout commantaire

--- BDD.sql
+++ BDD.sql
@@ -1,14 +1,14 @@
-create table joueur (
-idJoueur serial,
+create table joueur ( /*Création d'une table*/
+idJoueur serial, /*Creation des colones*/
 nom text,
 idEqui int
 );
 
 
 
-create table jeux (
+create table jeux ( /*Création d'une table*/
     
-    idJeux serial,
+    idJeux serial,/*Creation des colones*/
     scoreJ int,
     vie int,
 	idJoueur int
@@ -17,36 +17,36 @@ create table jeux (
     
 
 
-create table equipement (
-idEqui serial,
+create table equipement ( /*Création d'une table*/
+idEqui serial,/*Creation des colones*/
 nombreEqui int,
 nom text
 
 );
 
 
-create table bloc (
-    nbrBlock int,
+create table bloc ( /*Création d'une table*/
+    nbrBlock int,/*Creation des colones*/
     typeBlock text
 );
 
 
 
 
-INSERT Into public.joueur (idjoueur, nom, idequi) values (1034, 'Drake' , 0910);
+INSERT Into public.joueur (idjoueur, nom, idequi) values (1034, 'Drake' , 0910);/*insertion d'une valeur dans les colones d'une table */
 INSERT Into public.joueur (idjoueur, nom, idequi) values (1035, 'GarsYnov' , 1912);
 
-INSERT Into public.jeux (idjeux, scorej, vie, idjoueur) values (0100, 258 , 3,1034);
+INSERT Into public.jeux (idjeux, scorej, vie, idjoueur) values (0100, 258 , 3,1034);/*insertion d'une valeur dans les colones d'une table */
 INSERT Into public.jeux (idjeux, scorej, vie, idjoueur) values (0111, 208 , 3,1035);
 
-INSERT Into public.equipement (idequi, nombreequi, nom) values (0910, 1, 'missilex2');
+INSERT Into public.equipement (idequi, nombreequi, nom) values (0910, 1, 'missilex2');/*insertion d'une valeur dans les colones d'une table */
 INSERT Into public.equipement (idequi, nombreequi, nom) values (1912,NULL, NULL);
 
-INSERT Into public.bloc (nbrblock, typeblock) values (258,'vaisseau');
+INSERT Into public.bloc (nbrblock, typeblock) values (258,'vaisseau');/*insertion d'une valeur dans les colones d'une table */
 INSERT Into public.bloc (nbrblock, typeblock) values (2,'vaisseau');
 
 
-/*
+/* commande utiliser pour vérifier si les valeurs sont bien inseré.
 select * from jeux
 	INNER JOIN joueur on joueur.idjoueur = jeux.idjoueur
 	inner join equipement on equipement.idequi = joueur.idequi
